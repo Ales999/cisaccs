695c2eba375269857915ef46dde31879cef33912
Alexey Mekhanoshin <367257+Ales999@users.noreply.github.com>
2023-08-28 01:55:18 +0500

    Add SSH code

--- cisaccs.go
+++ cisaccs.go
@@ -1,9 +1,11 @@
 package cisaccs
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/ales999/cisaccs/internal/hostdata"
 	"github.com/ales999/cisaccs/internal/namedevs"
@@ -24,9 +26,6 @@ type CisAccount struct {
 
 func NewCisAccount(cisFileName string, pwdFileName string) *CisAccount {
 
-	// Open cisFile
-	//hostdatas := hostdata.New(cisFileName)
-
 	return &CisAccount{
 		initated:    true,        // Инициализировано через New(...)
 		cisFileName: cisFileName, // Файл с именами хостов, с указанием группы
@@ -34,7 +33,7 @@ func NewCisAccount(cisFileName string, pwdFileName string) *CisAccount {
 	}
 }
 
-func (a *CisAccount) OneCisExecuteSsh(host string, cmds []string) error {
+func (a *CisAccount) OneCisExecuteSsh(host string, port int, cmds []string) error {
 	// Проверка на корректность
 	if !a.initated {
 		return errors.New("create this struct by New command")
@@ -50,20 +49,65 @@ func (a *CisAccount) OneCisExecuteSsh(host string, cmds []string) error {
 		return errors.New("not found account")
 	}
 
+	// Debug print account info
 	fmt.Printf("hstAccount: %v\n", hstAccount)
+
+	// Настройка и подключение.
+	device, err := netrasp.New(host,
+		netrasp.WithDriver("ios"),
+		netrasp.WithSSHPort(port),
+		netrasp.WithUsernamePasswordEnableSecret(hstAccount.Username, hstAccount.Password, hstAccount.Secret),
+	)
+	if err != nil {
+		return fmt.Errorf("unable to init config: %v", err)
+	}
+	ctx, cancelOpen := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancelOpen()
+
+	err = device.Dial(ctx)
+	if err != nil {
+		//fmt.Printf("unable to connect: %v\n", err)
+		return fmt.Errorf("unable to connect: %v", err)
+	}
+	defer device.Close(context.Background())
+
+	ctx, cancelEnable := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancelEnable()
+
+	err = device.Enable(ctx)
+	if err != nil {
+		return fmt.Errorf("unable to Enable command: %v", err)
+	}
+
+	ctx, cancelRun := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancelRun()
+
+	fmt.Println("--------------------------------------------------")
+	for _, sendCommand := range cmds {
+		output, err := device.Run(ctx, sendCommand)
+		if err != nil {
+			fmt.Printf("unable to run command: %v\n", err)
+			continue
+		}
+		fmt.Print(output)
+	}
+	device.Close(ctx)
 
 	return nil
 }
 
-func (a *CisAccount) MultiCisExecuteSsh(hosts []string, cmds []string) error {
+func (a *CisAccount) MultiCisExecuteSsh(hosts []string, port int, cmds []string) error {
 	//
 	if !a.initated {
 		return errors.New("create this struct by New command")
 	}
+	if (port <= 0) || (port > 65534) {
+		return errors.New("ssh number port need > 0 and < 65534")
+	}
 
 	// Перебираем указанные хосты.
 	for _, host := range hosts {
-		a.OneCisExecuteSsh(host, cmds)
+		a.OneCisExecuteSsh(host, port, cmds)
 	}
 
 	return nil
