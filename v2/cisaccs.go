38e13321f6d485a4b25b2345df4cc2029865260d
Alexey Mekhanoshin <367257+Ales999@users.noreply.github.com>
2025-08-28 06:41:11 +0300

    First good with work

--- v2/cisaccs.go
+++ v2/cisaccs.go
@@ -237,15 +237,6 @@ func (ca *CisAccount) GetHostsDataByHostName() ([]*CND, error) {
 	return result, nil
 }
 
-/*
-// Структура содержит явки/пароли
-type HostData struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Secret   string `json:"secret"`
-}
-*/
-
 type HDP hostdata.HostData
 
 func newHostData(hd hostdata.HostData) HDP {
@@ -257,6 +248,9 @@ func newHostData(hd hostdata.HostData) HDP {
 }
 
 // GetHostDataByGroupName - вернуть данные для подключения из CT по имени группы в которой находится хост.
+//
+// ! Опасно так открыто выставлять по идее !
+// TODO: Подумать - а надо-ли!?
 func (ca *CisAccount) GetHostDataByGroupName(groupName string) (HDP, bool) {
 
 	hd, ok := hostdata.GetHostAccountByGroupName(ca.pwdFileName, groupName)
@@ -265,4 +259,114 @@ func (ca *CisAccount) GetHostDataByGroupName(groupName string) (HDP, bool) {
 	}
 
 	return HDP{}, false
+}
+
+// OneCisExecuteSshStepByStep - тестовая версия --> выполнить набор команд на одном хосте,
+// вывести вывод команд в консоль и вернуть все вместе.
+func (ca *CisAccount) OneCisExecuteSshStepByStep(hostName string, port int, cmds []string, flagUseCiscoWrire bool, connectTimeOut ...int) ([]string, error) {
+
+	// Приведем имя хоста к прописным буквам
+	hostName = strings.ToLower(strings.TrimSpace(hostName))
+	if len(hostName) == 0 {
+		return nil, errors.New("host name is empty")
+	}
+
+	var outs []string // результат работы выполнения на cisco
+	// Если необязательный параметр не указан то будем использовать его
+	var dialTimeout = 30
+	if len(connectTimeOut) > 0 {
+		dialTimeout = connectTimeOut[0]
+	}
+
+	// Проверка на корректность
+	if !ca.initated {
+		return outs, errors.New("not create this struct by NewCisAccount func")
+	}
+
+	var cnd namedevs.CiscoNameDevs
+	// Запросим данные о хосте по  его имени
+	hstData, err := cnd.GetHostDataByHostName(ca.cisFileName, hostName)
+	if err != nil {
+		return outs, err
+	}
+	// Запрос данных для авторизации на хосте по имени группы
+	hstAccount, found := hostdata.GetHostAccountByGroupName(ca.pwdFileName, hstData.Group)
+	if !found {
+		return outs, fmt.Errorf("error: not found account %s", hostName)
+	}
+
+	// Debug print account info
+	if cisDebug {
+		fmt.Printf("!Connect to host: %s (%v)", hostName, hstData.HostIp)
+	}
+
+	// Настройка и подключение.
+	device, err := netrasp.New(hstData.HostIp,
+		netrasp.WithDriver("ios"),
+		netrasp.WithSSHPort(port),
+		netrasp.WithDialTimeout(time.Duration(dialTimeout)*time.Second),
+		netrasp.WithUsernamePasswordEnableSecret(hstAccount.Username, hstAccount.Password, hstAccount.Secret),
+		netrasp.WithInsecureIgnoreHostKey(),
+	)
+	if err != nil {
+		return outs, fmt.Errorf("unable to init config: %v", err)
+	}
+	ctx, cancelOpen := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancelOpen()
+
+	err = device.Dial(ctx)
+	if err != nil {
+		return outs, fmt.Errorf("unable to connect: %v", err)
+	}
+	defer device.Close(context.Background())
+
+	ctxEnbl, cancelEnable := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancelEnable()
+
+	// TODO: If user is privilege 15 - not need enable
+	err = device.Enable(ctxEnbl)
+	if err != nil {
+		return outs, fmt.Errorf("unable to Enable command: %v", err)
+	}
+
+	ctx, cancelRun := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancelRun()
+	/*
+		if CisDebug {
+			fmt.Println(" - Done")
+		}
+	*/
+	fmt.Println("--------------------------------------------------")
+	smNum := len(hostName) + 18
+	sm := strings.Repeat(" ", smNum)
+
+	for _, sendCommand := range cmds {
+		sendCommand = strings.TrimLeft(sendCommand, " ")
+		outs = append(outs, sm+sendCommand)
+		output, err := device.Run(ctx, sendCommand)
+		if err != nil {
+			fmt.Printf("unable to run command: %v\n", err)
+			continue
+		}
+		multiouts := utils.ConvMultiStrToArrayStr(output)
+		outs = append(outs, multiouts...)
+	}
+	// Сохраняем конфигурацию если надо, после всех комманд
+	if flagUseCiscoWrire {
+		fmt.Print("Выполняю сохранение конфигурации на самой cisco ...")
+		_, err = device.Run(ctx, "wr mem")
+		if err != nil {
+			fmt.Printf("\nError: unable to run command wr mem:  %v\n", err)
+		} else {
+			fmt.Println(" - Выполнено.")
+		}
+	}
+	// Закрывем сессию.
+	ctxExit, cancelExit := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancelExit()
+	device.Run(ctxExit, "exit")
+
+	device.Close(ctx)
+
+	return outs, nil
 }
